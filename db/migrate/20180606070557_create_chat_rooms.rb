5a23cdbf584856e981a9d5cf49d78c07f2cafc96
baicalin <scutellariabaicalensis@gmail.com>
2018-08-24 18:41:06 +0900

    merging2

--- db/migrate/20180606070557_create_chat_rooms.rb
+++ db/migrate/20180606070557_create_chat_rooms.rb
@@ -3,8 +3,6 @@ class CreateChatRooms < ActiveRecord::Migration[5.2]
     create_table :chat_rooms do |t|
       t.string :title
       t.references :owner, foreign_key: true
-      t.float :lat
-      t.float :lng
 
       t.timestamps
     end
